64da3d92de9c80334571dc3772125c32b59da05d
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-01 22:00:42 +0700

    js lab 88

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-// write code here
+const arr = [2, 3, 5, 7, 11];
+// squareArr(arr); // [4, 9, 25, 49, 121]
+
+function squareArr(arr) {
+  return arr.map((item) => Math.pow(item, 2));
+}
+
+console.log(squareArr(arr));
